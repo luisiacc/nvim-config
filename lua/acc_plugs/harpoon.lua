44530ef1fb5eee40a29e135fcb3b36ceae75899b
luisiacc <lccpacc@gmail.com>
2021-12-31 14:13:08 -0500

    update telescope mappings

--- lua/acc_plugs/harpoon.lua
+++ lua/acc_plugs/harpoon.lua
@@ -1,7 +1,7 @@
 require("telescope").load_extension("harpoon")
 
 -- vim.cmd([[nnoremap <leader>h :lua require("harpoon.ui").toggle_quick_menu()<CR>]])
-vim.cmd([[nnoremap <leader>h :Telescope harpoon marks<CR>]])
+vim.cmd([[nnoremap <leader>h :Telescope harpoon marks path_display=tail<CR>]])
 vim.cmd([[nnoremap <leader>am :lua require("harpoon.mark").toggle_file()<CR>]])
 vim.cmd([[nnoremap <leader>an :lua require("harpoon.ui").nav_next()<CR>]])
 vim.cmd([[nnoremap <leader>ap :lua require("harpoon.ui").nav_prev()<CR>]])
