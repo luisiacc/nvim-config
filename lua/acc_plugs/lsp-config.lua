5e60edddbd3d8c33226d5f6ea342589cbe34389a
luisiacc <luiscorderoacc@gmail.com>
2022-01-29 12:34:34 -0500

    update lspconfig

--- lua/acc_plugs/lsp-config.lua
+++ lua/acc_plugs/lsp-config.lua
@@ -253,6 +253,7 @@ local server_configurations = {
         workspace = {
           -- Make the server aware of Neovim runtime files
           library = vim.api.nvim_get_runtime_file("", true),
+          checkThirdParty = false,
         },
         -- Do not send telemetry data containing a randomized but unique identifier
         telemetry = { enable = false },
