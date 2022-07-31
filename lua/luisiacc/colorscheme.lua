69d52d2e342b74bb096fa69aad5447114a41e5ea
luisiacc <luiscorderoacc@gmail.com>
2022-07-31 13:23:13 -0400

    italic and bold after colorscheme

--- lua/luisiacc/colorscheme.lua
+++ lua/luisiacc/colorscheme.lua
@@ -1,5 +1,10 @@
 local augroup = vim.api.nvim_create_augroup("ColorschemeChanges", {})
 
+function extend_hl(group, new_config)
+  local current_hl = vim.api.nvim_get_hl_by_name(group, true)
+  vim.api.nvim_set_hl(0, group, vim.tbl_extend("force", current_hl, new_config))
+end
+
 vim.api.nvim_create_autocmd("ColorScheme", {
   group = augroup,
   pattern = "*",
@@ -15,5 +20,7 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     for k, v in pairs(highs) do
       vim.api.nvim_set_hl(0, k, v)
     end
+    extend_hl("TSKeyword", { italic = true, bold = true })
+    extend_hl("TSKeywordFunction", { italic = true, bold = true })
   end,
 })
